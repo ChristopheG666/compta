2521477941aeb09ab694263d118b418530e52f95
ChristopheG666 <christophe.genova@gmail.com>
2023-12-04 12:19:46 +0100

    Add grouping

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ const requestListener = function(req, res) {
 
     // Parse the URL query. The leading '?' has to be removed before this.
     const query = parseQuery(url.search.substr(1));
-    //console.log(query);
+    console.log(query);
 
     try {
         switch (url.pathname) {
@@ -33,7 +33,25 @@ const requestListener = function(req, res) {
                     const contents = fs.readFileSync(__dirname + "/" + compa_filename);
                     res.setHeader("Content-Type", "application/json");
                     res.writeHead(200);
-                    res.end(contents);
+                    // Process the group
+                    expense = JSON.parse(contents);
+                    groupval = 0
+                    groupstart = -1
+                    for (var i = 0; i < expense.expense.length; i++) {
+                        if (expense.expense[i].grouped) {
+                            groupval += expense.expense[i].v
+                            if (groupstart == -1)
+                                groupstart = i
+                        }
+                    }
+                    console.log(expense.expense);
+                    if (groupstart >= 0) {
+                        expense.expense.splice(groupstart, expense.expense.length)
+                        expense.expense.push({'id': "group", 'l': "Groupe", 'v' : groupval, 'grouped': true})
+                    }
+                    
+                    console.log(expense.expense);
+                    res.end(JSON.stringify(expense));
                     break;
                 }
             case "/save":
@@ -60,6 +78,32 @@ const requestListener = function(req, res) {
                     res.end("Save Ok");
                     break;
                 }
+            case "/group":
+                {
+                    res.writeHead(200);
+
+                    const json = JSON.parse(query.json);
+                    // Read the current compta and group the expense until the id
+                    const contents = fs.readFileSync(__dirname + "/" + compa_filename);
+                    expense = JSON.parse(contents);
+                    // console.log(json);
+                    if (json == "group")
+                        // Ungroup
+                        for (var i = expense.expense.length-1; i >= 0; i--)
+                            delete expense.expense[i].grouped
+                    else
+                        for (var i = expense.expense.length-1; i >= 0; i--) {
+                            expense.expense[i].grouped = true
+                            if (expense.expense[i].id === json) {
+                                // We are done
+                                break;
+                            }
+                        }
+                    fs.writeFileSync(compa_filename, JSON.stringify(expense));
+
+                    res.end("Save Ok");
+                    break;
+                }
             case "/delete":
                 {
                     res.writeHead(200);
@@ -76,6 +120,7 @@ const requestListener = function(req, res) {
                             var spliced = expense.expense.splice(i, 1);
                             //console.log("Removed element: " + JSON.stringify(spliced));
                             //console.log("Remaining elements: " + JSON.stringify(expense.expense));
+                            break;
                         }
                     }
                     fs.writeFileSync(compa_filename, JSON.stringify(expense));
